c5dcd6d3c7060c7a296c3b7aa8ee4f63e6362901
MatthewDoran <doranma@tcd.ie>
2018-10-12 17:34:24 +0100

    Update Joption.py

--- Joption.py
+++ Joption.py
@@ -2,11 +2,24 @@ from tkinter import *
 from tkinter import ttk
 
 master = Tk()
-Label(master, text="First Name").grid(row=5, column= 10)
-Label(master, text="Daft.ie Bot").grid(row=3,column=10)
+master.title("Daft.ie Bot")
+master.configure(background='#9cd2bc')
+master.geometry("1000x1000")
 
-e1=Entry(master)
+Label(master, background="#9cd2bc", font = "Verdana 30 bold", text="Daft.ie Bot" ).place(x=520, y=20, anchor="center")
+Label(master, background="#9cd2bc", text="Name").place(x=500, y=50)
+Label(master, background="#9cd2bc", text="Email Address").place(x=487, y= 100)
+Label(master, background="#9cd2bc", text="Phone Number").place(x=487, y=150)
+Label(master, background="#9cd2bc", text="Your Message").place(x=490, y=200)
 
-e1.grid(row=5, column=11)
+e1=Entry(master, background='#ffdb6f')
+e2=Entry(master, background="#ffdb6f")
+e3=Entry(master, background="#ffdb6f")
+e4=Entry(master, background="#ffdb6f")
+
+e1.place(x=465, y=75)
+e2.place(x=465, y=125)
+e3.place(x=465, y=175)
+e4.place(x=465, y=225)
 
 mainloop()
