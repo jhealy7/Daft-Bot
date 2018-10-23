27e8858766da4e72e7d8f868e09dea4b13fd7cf2
MatthewDoran <doranma@tcd.ie>
2018-10-23 15:26:08 +0000

    Created daft_request
    Sends a request to daft and prints html. Still needs beautiful soup implementation

---
+++ daft_requests.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+fp = urllib.request.urlopen("https://www.daft.ie/dublin-city/residential-property-for-rent/south-dublin-city/?s%5Bmxp%5D=1650&s%5Badvanced%5D=1&s%5Bignored_agents%5D%5B0%5D=428&s%5Bignored_agents%5D%5B1%5D=1551&s%5Bsort_by%5D=date&s%5Bsort_type%5D=d&searchSource=rental")
+mybytes = fp.read()
+
+mystr = mybytes.decode("utf8")
+fp.close()
+
+print(mystr)
