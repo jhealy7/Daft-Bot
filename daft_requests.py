5102fae9de87b172f540c71583a07ba15633fd83
MatthewDoran <doranma@tcd.ie>
2018-10-28 15:54:14 +0000

    Implemented beautiful soup
    Implementation of beautiful soup

--- daft_requests.py
+++ daft_requests.py
@@ -1,9 +1,9 @@
+from bs4 import BeautifulSoup
 import urllib.request
 
 fp = urllib.request.urlopen("https://www.daft.ie/dublin-city/residential-property-for-rent/south-dublin-city/?s%5Bmxp%5D=1650&s%5Badvanced%5D=1&s%5Bignored_agents%5D%5B0%5D=428&s%5Bignored_agents%5D%5B1%5D=1551&s%5Bsort_by%5D=date&s%5Bsort_type%5D=d&searchSource=rental")
-mybytes = fp.read()
+soup = BeautifulSoup(fp,'html.parser')
 
-mystr = mybytes.decode("utf8")
 fp.close()
 
-print(mystr)
+print(soup.prettify())
