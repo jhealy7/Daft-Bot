23d1deeb97a2f4ba6459f79e1b75ba79796b52ca
MatthewDoran <doranma@tcd.ie>
2018-11-05 21:48:04 +0000

    Created bot and config
    Created bot to start finished app. Made a request function.  Tested and working.
    Config that will be needed for user information

---
+++ Config.py
@@ -0,0 +1,6 @@
+NAME = ""
+EMAIL = ""
+PHONE = ""
+MESSAGE = ""
+LINK_TO_MONITOR = 'https://www.daft.ie/dublin-city/residential-property-for-rent/dublin-city-centre,north-co-dublin,north-dublin-city,south-co-dublin,south-dublin-city,west-co-dublin/?s%5Bignored_agents%5D%5B0%5D=428&s%5Bignored_agents%5D%5B1%5D=1551&s%5Bsort_by%5D=date&s%5Bsort_type%5D=d&searchSource=rental'
+INTERVAL = 120  # How many seconds to wait before checking for new properties
