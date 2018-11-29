d138d3091f8f8f7ada1d8cf62b35e93ba09d19c1
MatthewDoran <35894231+MatthewDoran@users.noreply.github.com>
2018-11-29 16:21:17 +0000

    not required

--- daft_requests.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import urllib.request
-
-fp = urllib.request.urlopen("https://www.daft.ie/dublin-city/residential-property-for-rent/south-dublin-city/?s%5Bmxp%5D=1650&s%5Badvanced%5D=1&s%5Bignored_agents%5D%5B0%5D=428&s%5Bignored_agents%5D%5B1%5D=1551&s%5Bsort_by%5D=date&s%5Bsort_type%5D=d&searchSource=rental")
-soup = BeautifulSoup(fp,'html.parser')
-
-fp.close()
-print(soup.tr.h2.a)
-#print(soup.prettify())
